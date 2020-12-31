93b1b6d831a831ce1851dc9b5e85eadff37de002
yassineazrak <yassineazrak2019@gmail.com>
2020-12-31 23:04:42 +0100

    textblob

--- clean_data.py
+++ clean_data.py
@@ -6,32 +6,38 @@ from autocorrect import Speller
 from nltk.corpus import stopwords  
 from nltk.tokenize import word_tokenize  
 from nltk.stem import SnowballStemmer
+from textblob import TextBlob
+
 # snowball_stemmer = SnowballStemmer(‘english’)
 
 class Clean:
     def __init__(self,data):
         self.data = data
+        self.stem = SnowballStemmer('english')
 
     #remove punctuation 
     def remove_punctuation(self, word):
-        if word in string.punctuation:
-            return False
-        return True
+        list_word = []
+        for c in word:
+            if not c in string.punctuation:
+                list_word =.append(c)
+        return ''.join(list_word)
 
-    #tokenize text to word
+    #tokenize => split text to word 
     def tokenize(self,text):
-        if text:
-            return re.split('\W+',text)
-        return []
+        # if text:
+        #     return re.split('\W+',text)
+        # return []
+        return word_tokenize(text)
 
     #Stemming word
     def stemming_word(self, word):
-        pass
+        return self.stem(word)
 
     #clean url + emoji + html tags + numbers
     def cleaning_tags(self, text):
         stop_words = stopwords.words('english')  
-        new_text=re.sub(emoji.get_emoji_regexp(), r"", text)
+        new_text = re.sub(emoji.get_emoji_regexp(), r"", text)
         re.sub('<[^<]+?>','', text)
         return new_text
     
@@ -39,4 +45,7 @@ class Clean:
     def correct_word(self, word):
         spell = Speller(lang='en')
         return spell(word)
+    def translate(self, text)
+        text = TextBlob(text)
+        text.translate(to='en')
     
